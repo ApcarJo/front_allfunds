9710f0f7aa63a409e87f350e3fcaf0147f36c407
José Luis <hansokaz@gmail.com>
2022-04-11 21:56:31 +0200

    containers and components prepared

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Navigation from '../../components/Navigation/Navigation'
+
+const Home = () => {
+
+
+    
+        return (
+            <Navigation tittle="Hola, esto es título">
+
+            </Navigation>
+        )
+    }
+
+export default Home;
